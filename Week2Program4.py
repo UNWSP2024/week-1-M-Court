cc79be14af71e900f7ea57dde4b50353e8198487
M-Court <mcourt@students.unwsp.edu>
2024-09-12 14:09:11 -0500

    Update Week2Program4.py

---
+++ Week2Program4.py
@@ -0,0 +1,6 @@
+#ask for temperature in Celsius
+tempC = int(input("Enter temperature in degrees Celsius"))
+#convert to Fahrenheit
+tempF = ((9 / 5) * tempC) + 32
+#give temperature in Fahrenheit
+print(tempC, "degrees Celsius is", tempF, "degrees Fahrenheit")
